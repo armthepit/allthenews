ec00de372cc64c7171854bd99a5504aa1d17d702
John Nowinsky <armthepit@armthepit.com>
2016-12-27 12:34:04 -0600

    update comments

--- config/connection.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-
-// Local Database Configuration with Mongoose
-mongoose.connect("mongodb://localhost/goodnews", function(error)
-	{if(error) throw error;
-	console.log("Database connected");
-});
